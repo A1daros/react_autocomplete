6d35bac3aee617eef1ad8e56ea0d8619c215e893
Anatolii Lutai <toliklutai@gmail.com>
2026-07-15 16:00:50 +0300

    fix: Rework according to review requirements

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import { useState } from 'react';
 import './App.scss';
+import { Autocomplete } from './components/Autocomplete';
 import { peopleFromServer } from './data/people';
+// import { Autocomplete } from './components/Autocomplete';
 
 interface Person {
   name: string;
@@ -8,147 +10,25 @@ interface Person {
   died: number;
 }
 
-interface Props {
-  delay?: number;
-  onSelected?: (person: Person | null) => void;
-}
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const debounce = <T extends (...args: any[]) => void>(
-  callback: T,
-  delay: number,
-) => {
-  let timerId: ReturnType<typeof setTimeout> | null = null;
-
-  return (...args: Parameters<T>) => {
-    if (timerId) {
-      clearTimeout(timerId);
-    }
-
-    timerId = setTimeout(() => {
-      callback(...args);
-    }, delay);
-  };
-};
-
-export const App: React.FC<Props> = ({ delay = 300, onSelected }) => {
-  const [query, setQuery] = useState('');
-  const [isFocused, setIsFocused] = useState(false);
+export const App = () => {
   const [selectedPerson, setSelectedPerson] = useState<Person | null>(null);
-  const [debouncedQuery, setDebouncedQuery] = useState('');
-
-  const debouncedSetQuery = useMemo(() => {
-    return debounce((value: string) => setDebouncedQuery(value), delay);
-  }, [delay]);
-
-  useEffect(() => {
-    debouncedSetQuery(query);
-  }, [debouncedSetQuery, query]);
-
-  const filteredPeople = useMemo(() => {
-    if (!debouncedQuery) {
-      return peopleFromServer;
-    }
-
-    return peopleFromServer.filter(person => {
-      return person.name.toLowerCase().includes(debouncedQuery.toLowerCase());
-    });
-  }, [debouncedQuery]);
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value;
-
-    setQuery(value);
-
-    if (selectedPerson && selectedPerson.name !== value) {
-      setSelectedPerson(null);
-      onSelected?.(null);
-    }
-  };
-
-  const handleSelect = (person: Person) => {
-    setSelectedPerson(person);
-    setQuery(person.name);
-    onSelected?.(person);
-
-    setIsFocused(false);
-  };
-
-  const showDropdown =
-    isFocused && (query.trim() === '' || filteredPeople.length > 0);
-
-  const showNoResult =
-    isFocused && query.trim() !== '' && filteredPeople.length === 0;
-
-  const shouldShowSelected = selectedPerson !== null;
 
   return (
     <div className="container">
       <main className="section is-flex is-flex-direction-column">
         <h1 className="title" data-cy="title">
-          {shouldShowSelected ? (
+          {selectedPerson ? (
             `${selectedPerson.name} (${selectedPerson.born} - ${selectedPerson.died})`
           ) : (
             <p className="has-text-danger">No selected person</p>
           )}
         </h1>
 
-        <div className="dropdown is-active">
-          <div className="dropdown-trigger">
-            <input
-              type="text"
-              placeholder="Enter a part of the name"
-              data-cy="search-input"
-              value={query}
-              onChange={handleInputChange}
-              onFocus={() => setIsFocused(true)}
-              onBlur={() => setIsFocused(false)}
-              className="input"
-            />
-          </div>
-
-          {showDropdown && (
-            <div
-              className="dropdown-menu"
-              role="menu"
-              data-cy="suggestions-list"
-            >
-              <div className="dropdown-content">
-                {filteredPeople.map(person => (
-                  <div
-                    key={person.name}
-                    className="dropdown-item is-hovered"
-                    data-cy="suggestion-item"
-                    style={{ cursor: 'pointer' }}
-                    onMouseDown={() => handleSelect(person)}
-                  >
-                    <p
-                      className={`has-text-link ${selectedPerson?.name === person.name ? 'has-text-danger' : ''}`}
-                    >
-                      {person.name}
-                    </p>
-                  </div>
-                ))}
-              </div>
-            </div>
-          )}
-        </div>
-
-        {showNoResult && (
-          <div
-            className="
-            notification
-            is-danger
-            is-light
-            mt-3
-            is-align-self-flex-start
-          "
-            role="alert"
-            data-cy="no-suggestions-message"
-          >
-            <p className="has-text-danger">No matching suggestions</p>
-          </div>
-        )}
+        <Autocomplete
+          people={peopleFromServer}
+          onSelected={setSelectedPerson}
+          delay={300}
+        />
       </main>
     </div>
   );
